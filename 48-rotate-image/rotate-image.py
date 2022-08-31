7a5dc2e878785771dd77c5dbe018a97d5dc56f2e
asadbek <asadbekmirtazayev0@gmail.com>
2022-08-31 20:26:14 +0500

    Runtime: 79 ms Memory Usage: 14 MB

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        def roro(i,k):
+            for j in range(0,k):
+                t = matrix[i][i+j]
+                matrix[i][i+j] = matrix[n-i-1-j][i]
+                matrix[n-i-1-j][i] =  matrix[n-i-1][n-i-1-j]
+                matrix[n-i-1][n-i-1-j] = matrix[i+j][n-i-1]
+                matrix[i+j][n-i-1] = t
+        i=0
+        n=len(matrix)
+        k=n-1
+        while k>0:
+            roro(i,k)
+            i+=1
+            k-=2
+        return
