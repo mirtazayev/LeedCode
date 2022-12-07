0341052249d05179ebc3d4554d97a48cb1e6bb07
asadbek <asadbekmirtazayev0@gmail.com>
2022-12-07 10:30:34 +0500

    Runtime 1888 ms

---
+++ Easy/two-sum/two-sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums:
+                if i != nums.index(target - nums[i]):
+                    return [i, nums.index(target - nums[i])]
